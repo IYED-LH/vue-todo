6363d4e0d4447b3434864e3f8e2a7cee955451fb
IYED-LH <iyedhmidi@gmail.com>
2022-06-24 00:18:56 +0100

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,49 @@
+<template>
+<div :class= "[task.completed ? 'done' : 'undone', 'task']">
+
+<h3>{{task.title}} 
+<i class="fas fa-times"></i>
+</h3>
+
+<p>{{task.Date}}</p>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Task',
+    props: {
+        task: Object,
+    }, 
+}
+
+</script>
+
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.done {
+  border-left: 5px solid green;
+}
+
+.undone {
+  border-left: 5px solid rgb(195, 0, 0);
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  }
+
+</style>
